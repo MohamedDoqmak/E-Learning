a76b21be437d680ca02cba661f414a6e463259a5
mohamedAyman <modyayman2001@gmail.com>
2023-12-09 17:32:51 +0200

    Text direction based on Language

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        View::composer('*', function ($view) {
+            $langDirection = [
+                'en' => 'ltr',
+                'ar' => 'rtl',
+                // Add other languages and their respective directions as needed
+            ];
+
+            $view->with('langDirection', $langDirection);
+        });
     }
 }
