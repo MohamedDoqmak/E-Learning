b833d977cea50a192becb8d202f281b0cba5ddeb
mohamedAyman <modyayman2001@gmail.com>
2024-02-14 21:29:16 +0200

    Category Translation

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreCategoryRequest;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+    public function create()
+    {
+        return view('categories.create');
+    }
+    public function store(StoreCategoryRequest $request)
+    {
+        Category::create($request->validated());
+        return redirect('categories/create');
+    }
+    public function show($id)
+    {
+        //
+    }
+    public function edit($id)
+    {
+        //
+    }
+    public function update(Request $request, $id)
+    {
+        //
+    }
+    public function destroy($id)
+    {
+        //
+    }
+}
